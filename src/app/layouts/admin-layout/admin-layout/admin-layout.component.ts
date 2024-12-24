723c182e58fc66a28a54c00e733372f967f27c47
Ramakrishna <ram.siginam@gmail.com>
2024-12-24 23:09:15 +0530

    done

--- src/app/layouts/admin-layout/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout/admin-layout.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
-  standalone: true,
-  imports: [],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
